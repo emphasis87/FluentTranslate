50aff866c4309e2cbe97f57efb41be1849f9374c
lubos_hemala <lubos.hemala@gmail.com>
2020-08-27 13:35:13 +0200

    tests

--- src/FluentTranslate.Parser.Tests/FluentVisitorTests.cs
+++ src/FluentTranslate.Parser.Tests/FluentVisitorTests.cs
@@ -1,158 +1,172 @@
 ﻿using System;
+using System.IO;
+using Antlr4.Runtime;
 using NUnit.Framework;
 
 namespace FluentTranslate.Parser.Tests
 {
 	public class FluentVisitorTests
 	{
+		private void Act(string resource)
+		{
+			var inputStream = new AntlrInputStream(new StringReader(resource));
+			var lexer = new FluentLexer(inputStream);
+
+			var tokenStream = new CommonTokenStream(lexer);
+			var parser = new FluentParser(tokenStream);
+
+			var visitor = new FtlVisitor();
+			visitor.Visit(parser.resource());
+		}
+
 		[Test]
 		public void Hello()
 		{
-
+			Act(Resources.Hello);
 		}
 
 		[Test]
 		public void Attributes()
 		{
-
+			Act(Resources.Attributes);
 		}
 
 		[Test]
 		public void Comments()
 		{
-
+			Act(Resources.Comments);
 		}
 
 		[Test]
 		public void Functions()
 		{
-
+			Act(Resources.Functions);
 		}
 
 		[Test]
 		public void FunctionsDatetime()
 		{
-
+			Act(Resources.FunctionsDatetime);
 		}
 
 		[Test]
 		public void FunctionsNumber()
 		{
-
+			Act(Resources.FunctionsNumber);
 		}
 
 		[Test]
 		public void MultilineText()
 		{
-
+			Act(Resources.MultilineText);
 		}
 
 		[Test]
 		public void Placeables()
 		{
-
+			Act(Resources.Placeables);
 		}
 
 		[Test]
 		public void PlaceablesInterpolation()
 		{
-
+			Act(Resources.PlaceablesInterpolation);
 		}
 
 		[Test]
 		public void PlaceablesSpecialCharacters()
 		{
-
+			Act(Resources.PlaceablesSpecialCharacters);
 		}
 
 		[Test]
 		public void QuotedText()
 		{
-
+			Act(Resources.QuotedText);
 		}
 
 		[Test]
 		public void QuotedTextEscape()
 		{
-
+			Act(Resources.QuotedTextEscape);
 		}
 
 		[Test]
 		public void QuotedTextLeadingBracket()
 		{
-
+			Act(Resources.QuotedTextLeadingBracket);
 		}
 
 		[Test]
 		public void QuotedTextLeadingDot()
 		{
-
+			Act(Resources.QuotedTextLeadingDot);
 		}
 
 		[Test]
 		public void QuotedTextUnicodeDash()
 		{
-
+			Act(Resources.QuotedTextUnicodeDash);
 		}
 
 		[Test]
 		public void QuotedTextUnicodeEscape()
 		{
-
+			Act(Resources.QuotedTextUnicodeEscape);
 		}
 
 		[Test]
 		public void Selectors()
 		{
-
+			Act(Resources.Selectors);
 		}
 
 		[Test]
 		public void SelectorsNumber()
 		{
-
+			Act(Resources.SelectorsNumber);
 		}
 
 		[Test]
 		public void SelectorsOrdinal()
 		{
-
+			Act(Resources.SelectorsOrdinal);
 		}
 
 		[Test]
 		public void TermsAttributes()
 		{
-
+			Act(Resources.TermsAttributes);
 		}
 
 		[Test]
 		public void TermsParameterized()
 		{
-
+			Act(Resources.TermsParameterized);
 		}
 
 		[Test]
 		public void TermsVariants()
 		{
-
+			Act(Resources.TermsVariants);
 		}
 
 		[Test]
 		public void Variables()
 		{
-
+			Act(Resources.Variables);
 		}
 
 		[Test]
 		public void VariablesExplicitFormatting()
 		{
-
+			Act(Resources.VariablesExplicitFormatting);
 		}
 
 		[Test]
 		public void VariablesImplicitFormatting()
 		{
-
+			Act(Resources.VariablesImplicitFormatting);
 		}
 	}
 }
--- src/FluentTranslate.Parser/FluentContext.cs
+++ src/FluentTranslate.Parser/FluentContext.cs
@@ -1,9 +1,14 @@
-﻿using FluentTranslate.Common.Domain;
+﻿using Antlr4.Runtime;
+using FluentTranslate.Common.Domain;
 
 namespace FluentTranslate.Parser
 {
 	public class FluentContext : Antlr4.Runtime.ParserRuleContext
 	{
 		public IFtlElement Element { get; set; }
+
+		public FluentContext(ParserRuleContext parent, int invokingStateNumber) : base(parent, invokingStateNumber)
+		{
+		}
 	}
 }
