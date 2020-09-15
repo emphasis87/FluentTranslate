3cc35fd020e2032c872a0faad67fc140d7446fc0
Luboš Hemala <lubos.hemala@gmail.com>
2020-09-15 17:22:17 +0200

    translator

---
+++ src/FluentTranslate.Common/FluentEngine.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using FluentTranslate.Common.Domain;
+
+namespace FluentTranslate.Common
+{
+    public interface IFluentEngine
+    {
+        IFluentContainer FindByReference(string reference);
+        object Call(string reference, IList<(string name, object value)> arguments);
+    }
+
+    public class FluentEngine : IFluentEngine
+    {
+        public IFluentContainer FindByReference(string reference)
+        {
+            
+        }
+
+        public object Call(string reference, IList<(string name, object value)> arguments)
+        {
+            
+        }
+    }
+}
---
+++ src/FluentTranslate.Common/FluentEvaluationContext.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using FluentTranslate.Common.Domain;
+
+namespace FluentTranslate.Common
+{
+    public class FluentEvaluationContext
+    {
+
+
+        public object ResolveVariable(string id)
+        {
+
+        }
+
+        public void BeginScope(IEnumerable<(string name, object value)> arguments)
+        {
+            
+        }
+
+        public void CloseScope()
+        {
+            
+        }
+    }
+}
