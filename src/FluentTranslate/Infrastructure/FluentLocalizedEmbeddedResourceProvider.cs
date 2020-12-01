22209227ceeb9e331d8bf2cba999c251fb3f8583
lubos_hemala <lubos.hemala@gmail.com>
2020-12-01 18:55:02 +0100

    embedded resource provider, project nettstandard2.0 target

--- src/FluentTranslate/Infrastructure/FluentLocalizedEmbeddedResourceProvider.cs
+++ src/FluentTranslate/Infrastructure/FluentLocalizedEmbeddedResourceProvider.cs
@@ -1,21 +1,105 @@
-﻿using System.Reflection;
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Threading.Tasks;
+using FluentTranslate.Domain;
 
 namespace FluentTranslate.Infrastructure
 {
-	public class FluentLocalizedEmbeddedResourceProvider : FluentLocalizedFileResourceProvider
+	public class FluentLocalizedEmbeddedResourceProvider : FluentPollingResourceProvider
 	{
-		public Assembly Assembly { get; }
+		public Assembly RootAssembly { get; }
+		public string RootPath { get; }
 
-		public FluentLocalizedEmbeddedResourceProvider(Assembly assembly, string rootPath, IFluentConfiguration configuration = null) 
-			: base(rootPath, configuration)
+		private readonly ConcurrentDictionary<(Assembly, string), IFluentResourceProvider> _providerByAssemblyAndPath =
+			new ConcurrentDictionary<(Assembly, string), IFluentResourceProvider>();
+
+		public FluentLocalizedEmbeddedResourceProvider(Assembly assembly, string rootPath, IFluentConfiguration configuration = null)
+			: base(configuration)
 		{
-			Assembly = assembly;
+			RootAssembly = assembly;
+			RootPath = rootPath;
+
+			IsLocalized = true;
 			IsPollingEnabled = false;
 		}
 
-		protected override IFluentResourceProvider CreateProvider(string path)
+		protected virtual IEnumerable<(Assembly, string)> GetPaths(CultureInfo culture)
 		{
-			return new FluentEmbeddedResourceProvider(Assembly, path, Configuration);
+			var extension = Path.GetExtension(RootPath);
+
+			if (!string.IsNullOrWhiteSpace(culture.Name))
+			{
+				var pathWithLongCulture = Path.ChangeExtension(RootPath, $"{culture.Name}{extension}");
+				yield return (RootAssembly, pathWithLongCulture);
+			}
+
+			var pathWithShortCulture = Path.ChangeExtension(RootPath, $"{culture.TwoLetterISOLanguageName}{extension}");
+			yield return (RootAssembly, pathWithShortCulture);
+
+#if NETFRAMEWORK || NETSTANDARD2_0
+			Assembly satellite = null;
+			try
+			{
+				satellite = RootAssembly.GetSatelliteAssembly(culture);
+			}
+			catch { }
+			
+			if (satellite != null)
+				yield return (satellite, RootPath);
+#endif
+
+			yield return (RootAssembly, RootPath);
+		}
+
+		protected virtual IFluentCompositeResourceProvider CreateProvider(CultureInfo culture)
+		{
+			var compositeProvider = new FluentCompositeResourceProvider(Configuration);
+			var parameters = GetPaths(culture).Distinct().ToArray();
+			foreach (var (assembly, path) in parameters)
+			{
+				if (!_providerByAssemblyAndPath.TryGetValue((assembly, path), out var provider))
+				{
+					provider = CreateProvider(assembly, path);
+					provider = _providerByAssemblyAndPath.GetOrAdd((assembly, path), provider);
+				}
+				compositeProvider.Add(provider);
+			}
+
+			return compositeProvider;
+		}
+
+		protected virtual IFluentResourceProvider CreateProvider(Assembly assembly, string path)
+		{
+			return new FluentEmbeddedResourceProvider(assembly, path, Configuration);
+		}
+
+		protected override async Task<FluentResource> FindResourceAsync(Context context, CultureInfo culture)
+		{
+			var ctx = (LocalizedFileContext)context;
+			var compositeProvider = ctx.Provider;
+			var next = await compositeProvider.GetResourceAsync(culture);
+			return next;
+		}
+
+		protected override Context CreateContext(CultureInfo culture)
+		{
+			var compositeProvider = CreateProvider(culture);
+			return new LocalizedFileContext(compositeProvider, culture);
+		}
+
+		protected class LocalizedFileContext : Context
+		{
+			public IFluentCompositeResourceProvider Provider { get; }
+
+			public LocalizedFileContext(IFluentCompositeResourceProvider provider, CultureInfo culture) : base(culture)
+			{
+				Provider = provider;
+			}
 		}
 	}
 }
