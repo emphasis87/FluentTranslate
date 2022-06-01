bb91d9a94473e6bf201c9bb4fc2b602b93fdcd68
lubos_hemala <lubos.hemala@gmail.com>
2022-06-01 10:37:15 +0200

    aggregator

---
+++ src/FluentTranslate.Parser/FluentElementAggregator.cs
@@ -0,0 +1,15 @@
+﻿using FluentTranslate.Domain;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FluentTranslate.Parser
+{
+    public class FluentElementAggregator
+    {
+        public static bool TryAggregate(IFluentElement item0, IFluentElement item1)
+        {
+
+        }
+    }
+}
