32ca61c86c370618616886bba82704aa45399e1e
lubos_hemala <lubos.hemala@gmail.com>
2022-06-04 11:50:39 +0200

    refactoring

--- src/FluentTranslate/Common/FluentAggregator.cs
+++ src/FluentTranslate/Common/FluentAggregator.cs
@@ -10,7 +10,7 @@ namespace FluentTranslate.Common
 {
     public class FluentAggregator
     {
-        public static bool CanAggregate(IFluentElement e1, IFluentElement e2)
+        public virtual bool CanAggregate(IFluentElement e1, IFluentElement e2)
         {
             if (e1 is null || e2 is null) return true;
             if (ReferenceEquals(e1, e2)) return true;
@@ -24,12 +24,12 @@ namespace FluentTranslate.Common
             };
         }
 
-        public static bool CanAggregate(IEnumerable<IFluentElement> elements!!)
+        public virtual bool CanAggregate(IEnumerable<IFluentElement> elements!!)
         {
             return ZipOrDefault(elements, elements.Skip(1), CanAggregate).All(r => r);
         }
 
-        public static IFluentElement Aggregate(IEnumerable<IFluentElement> elements!!)
+        public virtual IFluentElement Aggregate(IEnumerable<IFluentElement> elements!!)
         {
             var all = elements.Where(c => c is not null).ToArray();
             if (all.Length == 0) return default;
@@ -41,30 +41,46 @@ namespace FluentTranslate.Common
             switch (first)
             {
                 case FluentEmptyLines empty:
-                    empty.Count += all.Cast<FluentEmptyLines>().Sum(x => x.Count);
-                    return empty;
+                    return AggregateEmptyLines(all, empty);
                 case FluentText text:
-                    var texts = all.Cast<FluentText>().Select(x => x.Value).Where(v => v is not null).ToArray();
-                    var value = string.Join("\r\n", texts);
-                    text.Value = value;
-                    return text;
+                    return AggregateText(all, text);
                 case FluentComment comment1:
-                    var comments = all.OfType<FluentComment>().Select(x => x.Value);
-                    if (last is FluentRecord r1)
-                        comments = comments.Concat(new[] { r1.Comment });
-                    var comment = string.Join("\r\n", comments.Where(x => x is not null));
-                    if (last is FluentRecord r2)
-                    {
-                        r2.Comment = comment;
-                        return r2;
-                    }
-                    else
-                    {
-                        comment1.Value = comment;
-                        return comment1;
-                    }
+                    return AggregateComments(all, comment1);
                 default:
                     return default;
+            }
+        }
+
+        public virtual IFluentElement AggregateEmptyLines(IFluentElement[] elements!!, FluentEmptyLines first!!)
+        {
+            first.Count = elements.Cast<FluentEmptyLines>().Sum(x => x.Count);
+            return first;
+        }
+
+        public virtual IFluentElement AggregateText(IFluentElement[] elements!!, FluentText first!!)
+        {
+            var texts = elements.Cast<FluentText>().Select(x => x.Value).Where(v => v is not null);
+            var value = string.Join("\r\n", texts);
+            first.Value = value;
+            return first;
+        }
+
+        public virtual IFluentElement AggregateComments(IFluentElement[] elements!!, FluentComment first!!)
+        {
+            var last = elements[elements.Length - 1];
+            var comments = elements.OfType<FluentComment>().Select(x => x.Value);
+            if (last is FluentRecord r1)
+                comments = comments.Concat(new[] { r1.Comment });
+            var comment = string.Join("\r\n", comments.Where(x => x is not null));
+            if (last is FluentRecord r2)
+            {
+                r2.Comment = comment;
+                return r2;
+            }
+            else
+            {
+                first.Value = comment;
+                return first;
             }
         }
     }
